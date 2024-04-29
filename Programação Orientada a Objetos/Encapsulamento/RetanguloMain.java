63e515578c0be7f3d4850227175176d919f98906
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:45:06 -0300

    Update RetanguloMain.java

--- Programação Orientada a Objetos/Encapsulamento/RetanguloMain.java
+++ Programação Orientada a Objetos/Encapsulamento/RetanguloMain.java
@@ -1,10 +1,25 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class RetanguloMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Informe a altura do retângulo:");
+        double altura = sc.nextDouble();
+        
+        System.out.println("Informe a base do retângulo:");
+        double base = sc.nextDouble();
+        
+        Retangulo retangulo = new Retangulo(altura, base);
+        
+        System.out.println("Altura do Retângulo: " + retangulo.getAltura());
+        System.out.println("Base do Retângulo: " + retangulo.getBase());
+        
+        System.out.println("Área do Retângulo: " + retangulo.calcularArea());
+        
+        sc.close();
+    }
 }

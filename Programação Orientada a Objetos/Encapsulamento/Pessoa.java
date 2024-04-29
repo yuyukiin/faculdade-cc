a5b01306f2828b142fe72b5730d70e653a862300
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:51:35 -0300

    Update Pessoa.java

--- Programação Orientada a Objetos/Encapsulamento/Pessoa.java
+++ Programação Orientada a Objetos/Encapsulamento/Pessoa.java
@@ -1,5 +1,44 @@
 package Exercícios;
 
-public class Pessoa {
+import java.util.Scanner;
 
+public class Pessoa {
+    private String nome;
+    private int idade;
+    private long cpf;
+
+    public Pessoa(String nome, int idade, long cpf) {
+        super();
+        this.nome = nome;
+        this.idade = idade;
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+    
+    public boolean verificaMaiorDeIdade() {
+        return idade >= 18;
+    }
 }

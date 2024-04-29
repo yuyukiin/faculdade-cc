07073f1402a716415432e90fe288b8ed9167fcc1
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:53:12 -0300

    Update ContaBancaria.java

--- Programação Orientada a Objetos/Encapsulamento/ContaBancaria.java
+++ Programação Orientada a Objetos/Encapsulamento/ContaBancaria.java
@@ -1,33 +1,38 @@
 package Exercícios;
 
 public class ContaBancaria {
-	private double saldo;
-	private double limite;
-	
-	public ContaBancaria(double saldo, double limite) {
-		super();
-		this.saldo = saldo;
-		this.limite = limite;
-	}
+    private double saldo;
+    private double limite;
+    
+    public ContaBancaria(double saldo, double limite) {
+        super();
+        this.saldo = saldo;
+        this.limite = limite;
+    }
 
-	public double getSaldo() {
-		return saldo;
-	}
+    public double getSaldo() {
+        return saldo;
+    }
 
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
 
-	public double getLimite() {
-		return limite;
-	}
+    public double getLimite() {
+        return limite;
+    }
 
-	public void setLimite(double limite) {
-		this.limite = limite;
-	}
-	
-	public double Saque() {
-		
-	}
-	
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+    
+    public boolean saque(double valor) {
+        if (valor <= saldo + limite) {
+            saldo -= valor;
+            return true;
+        } else {
+            System.out.println("Saldo insuficiente para realizar o saque.");
+            return false;
+        }
+    }
 }

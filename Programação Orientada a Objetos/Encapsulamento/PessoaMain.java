460f2d5a3abc4975735e1a4ccaa09215909aa72c
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:51:43 -0300

    Update PessoaMain.java

--- Programação Orientada a Objetos/Encapsulamento/PessoaMain.java
+++ Programação Orientada a Objetos/Encapsulamento/PessoaMain.java
@@ -1,22 +1,33 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class PessoaMain {
 
-	public static void main(String[] args) {
-		Pessoa p = new Pessoa ("Yuri", 21, 020);
-		
-		System.out.println("Yuri: ");
-		System.out.println(p.getNome());
-		System.out.println(p.getIdade());
-		System.out.println(p.getCpf());
-		p.verificaMaiorDeldade();
-	
-		System.out.println("Gilberto: ");
-		p.setNome("Gigi");
-		p.setIdade(19);
-		p.setCpf(020);
-		p.verificaMaiorDeldade();	
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Informe o nome da pessoa:");
+        String nome = sc.nextLine();
+        
+        System.out.println("Informe a idade da pessoa:");
+        int idade = sc.nextInt();
+        
+        System.out.println("Informe o CPF da pessoa:");
+        long cpf = sc.nextLong();
+        
+        Pessoa p = new Pessoa(nome, idade, cpf);
+        
+        System.out.println("Nome da Pessoa: " + p.getNome());
+        System.out.println("Idade da Pessoa: " + p.getIdade());
+        System.out.println("CPF da Pessoa: " + p.getCpf());
+        
+        if(p.verificaMaiorDeIdade()) {
+            System.out.println("A pessoa é maior de idade.");
+        } else {
+            System.out.println("A pessoa não é maior de idade.");
+        }
+        
+        sc.close();
+    }
 }

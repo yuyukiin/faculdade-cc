f51ada152bfd1da676d06a40c2bb63bfb674ba80
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:45:19 -0300

    Update Retangulo.java

--- Programação Orientada a Objetos/Encapsulamento/Retangulo.java
+++ Programação Orientada a Objetos/Encapsulamento/Retangulo.java
@@ -1,33 +1,34 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class Retangulo {
-	private double altura;
-	private double base;
-	
-	public Retangulo(double altura, double base) {
-		super();
-		this.altura = altura;
-		this.base = base;
-	}
+    private double altura;
+    private double base;
+    
+    public Retangulo(double altura, double base) {
+        super();
+        this.altura = altura;
+        this.base = base;
+    }
 
-	public double getAltura() {
-		return altura;
-	}
+    public double getAltura() {
+        return altura;
+    }
 
-	public void setAltura(double altura) {
-		this.altura = altura;
-	}
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
 
-	public double getBase() {
-		return base;
-	}
+    public double getBase() {
+        return base;
+    }
 
-	public void setBase(double base) {
-		this.base = base;
-	}
-	
-	public double CalcularArea() {
-		return (base*altura);
-	}
-	
+    public void setBase(double base) {
+        this.base = base;
+    }
+    
+    public double calcularArea() {
+        return base * altura;
+    }
 }

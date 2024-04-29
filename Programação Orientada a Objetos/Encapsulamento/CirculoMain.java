958883a1fd6e62467ef1f8c28b95e6bd9d8b4a82
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:41:26 -0300

    Update CirculoMain.java

--- Programação Orientada a Objetos/Encapsulamento/CirculoMain.java
+++ Programação Orientada a Objetos/Encapsulamento/CirculoMain.java
@@ -1,15 +1,20 @@
 package Exercícios;
+import java.util.Scanner;
 
 public class CirculoMain {
 
-	public static void main(String[] args) {
-		Circulo c = new Circulo(2);
-		
-		System.out.println(c.getRaio());
-		c.setRaio(6);
-		
-		System.out.println("Área do Circulo: " + c.calculeArea());
-
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Informe o raio do círculo:");
+        double raio = sc.nextDouble();
+        
+        Circulo c = new Circulo(raio);
+        
+        System.out.println("Raio do Círculo: " + c.getRaio());
+        
+        System.out.println("Área do Círculo: " + c.calculaArea());
+        
+        sc.close();
+    }
 }

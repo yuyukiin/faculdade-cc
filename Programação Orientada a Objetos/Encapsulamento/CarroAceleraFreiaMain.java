6dc063bfb8408f0bb4c2e94f260f862c065d68c6
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:55:53 -0300

    Update CarroAceleraFreiaMain.java

--- Programação Orientada a Objetos/Encapsulamento/CarroAceleraFreiaMain.java
+++ Programação Orientada a Objetos/Encapsulamento/CarroAceleraFreiaMain.java
@@ -2,9 +2,16 @@ package Exercícios;
 
 public class CarroAceleraFreiaMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        CarroAceleraFreia carro = new CarroAceleraFreia("ABC1234");
+        
+        System.out.println("Placa do Carro: " + carro.getPlaca());
+        System.out.println("Velocidade do Carro: " + carro.getVelocidade());
+        
+        carro.acelerar(50);
+        System.out.println("Velocidade do Carro após acelerar: " + carro.getVelocidade());
+        
+        carro.freiar(20);
+        System.out.println("Velocidade do Carro após freiar: " + carro.getVelocidade());
+    }
 }

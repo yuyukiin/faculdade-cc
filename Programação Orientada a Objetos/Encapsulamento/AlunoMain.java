7d56544811e350625bc0cbbb0aff83d26b54a009
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:38:53 -0300

    Update AlunoMain.java

--- Programação Orientada a Objetos/Encapsulamento/AlunoMain.java
+++ Programação Orientada a Objetos/Encapsulamento/AlunoMain.java
@@ -1,32 +1,33 @@
 package Exemplos;
+import java.util.Scanner;
 
 public class AlunoMain {
 
-	public static void main(String[] args) {
-		Aluno aluno = new Aluno("Yuri", 6, 5, 9);
-		
-		
-		System.out.println("Notas");
-		System.out.println(aluno.getNome());
-		System.out.println(aluno.getNota1());
-		System.out.println(aluno.getNota2());
-		System.out.println(aluno.getNota3());
-		
-		System.out.println("Media Da Yuri: " + aluno.calculaMedia());
-		
-		aluno.setNome("Angela");
-		aluno.setNota1(6);
-		aluno.setNota2(7);
-		aluno.setNota3(9);
-		
-		System.out.println("Notas");
-		System.out.println(aluno.getNome());
-		System.out.println(aluno.getNota1());
-		System.out.println(aluno.getNota2());
-		System.out.println(aluno.getNota3());
-		
-		System.out.println("Media Da Angela: " + aluno.calculaMedia());
-		
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Informe o nome do aluno:");
+        String nome = sc.nextLine();
+        
+        System.out.println("Informe a nota 1:");
+        int nota1 = sc.nextInt();
+        
+        System.out.println("Informe a nota 2:");
+        int nota2 = sc.nextInt();
+        
+        System.out.println("Informe a nota 3:");
+        int nota3 = sc.nextInt();
+        
+        Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
 
+        System.out.println("Notas");
+        System.out.println(aluno.getNome());
+        System.out.println(aluno.getNota1());
+        System.out.println(aluno.getNota2());
+        System.out.println(aluno.getNota3());
+
+        System.out.println("Média do(a) " + aluno.getNome() + ": " + aluno.calculaMedia());
+
+        sc.close();
+    }
 }

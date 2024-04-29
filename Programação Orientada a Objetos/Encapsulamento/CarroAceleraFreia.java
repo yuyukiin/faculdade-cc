d67f411ea698c89c0d57a6bfb2d4800db97b595c
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:55:32 -0300

    Update CarroAceleraFreia.java

--- Programação Orientada a Objetos/Encapsulamento/CarroAceleraFreia.java
+++ Programação Orientada a Objetos/Encapsulamento/CarroAceleraFreia.java
@@ -1,5 +1,35 @@
 package Exercícios;
 
 public class CarroAceleraFreia {
+    private String placa;
+    private int velocidade;
 
+    public CarroAceleraFreia(String placa) {
+        this.placa = placa;
+        this.velocidade = 0;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public void acelerar(int valor) {
+        this.velocidade += valor;
+    }
+
+    public void freiar(int valor) {
+        if (this.velocidade - valor >= 0) {
+            this.velocidade -= valor;
+        } else {
+            this.velocidade = 0;
+        }
+    }
 }

6e1c7c5e24728d60eaf90745b6d90830df712eac
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:42:16 -0300

    Update Circulo.java

--- Programação Orientada a Objetos/Encapsulamento/Circulo.java
+++ Programação Orientada a Objetos/Encapsulamento/Circulo.java
@@ -1,23 +1,22 @@
 package Exercícios;
 
 public class Circulo {
-	private double raio;
+    private double raio;
 
-	public Circulo(double raio) {
-		super();
-		this.raio = raio;
-	}
+    public Circulo(double raio) {
+        super();
+        this.raio = raio;
+    }
 
-	public double getRaio() {
-		return raio;
-	}
+    public double getRaio() {
+        return raio;
+    }
 
-	public void setRaio(double raio) {
-		this.raio = raio;
-	}
-	
-	public double calculeArea() {
-		return (2*3.16*raio);
-	}
-	
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+    
+    public double calculaArea() {
+        return Math.PI * raio * raio;
+    }
 }

a508062830af010dfb9cf57e958cae36667dfcc0
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:48:22 -0300

    Update Triangulo.java

--- Programação Orientada a Objetos/Encapsulamento/Triangulo.java
+++ Programação Orientada a Objetos/Encapsulamento/Triangulo.java
@@ -1,46 +1,42 @@
 package Exercícios;
 
+import java.util.Scanner;
+
 public class Triangulo {
-	private double ladoA, ladoB, ladoC;
+    private double ladoA, ladoB, ladoC;
 
-	public Triangulo(double ladoA, double ladoB, double ladoC) {
-		super();
-		this.ladoA = ladoA;
-		this.ladoB = ladoB;
-		this.ladoC = ladoC;
-	}
+    public Triangulo(double ladoA, double ladoB, double ladoC) {
+        super();
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
 
-	public double getLadoA() {
-		return ladoA;
-	}
+    public double getLadoA() {
+        return ladoA;
+    }
 
-	public void setLadoA(double ladoA) {
-		this.ladoA = ladoA;
-	}
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
 
-	public double getLadoB() {
-		return ladoB;
-	}
+    public double getLadoB() {
+        return ladoB;
+    }
 
-	public void setLadoB(double ladoB) {
-		this.ladoB = ladoB;
-	}
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
 
-	public double getLadoC() {
-		return ladoC;
-	}
+    public double getLadoC() {
+        return ladoC;
+    }
 
-	public void setLadoC(double ladoC) {
-		this.ladoC = ladoC;
-	}
-	
-	public void verificaEquilatero() {
-		if(this.ladoA == this.ladoB && this.ladoA == this.ladoC && this.ladoB == this.ladoC) {
-			System.out.println("Triângulo é equilátero");
-		}else {
-			System.out.println("Triângulo não é equilátero");
-		}
-		return;
-	}
-	
+    public void setLadoC(double ladoC) {
+        this.ladoC = ladoC;
+    }
+    
+    public boolean verificaEquilatero() {
+        return ladoA == ladoB && ladoA == ladoC && ladoB == ladoC;
+    }
 }

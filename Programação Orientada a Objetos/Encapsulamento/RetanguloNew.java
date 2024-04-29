3d4df370ee32b6d4b2453c5e0dbacbfa37a7de9f
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-04-29 07:54:19 -0300

    Update RetanguloNew.java

--- Programação Orientada a Objetos/Encapsulamento/RetanguloNew.java
+++ Programação Orientada a Objetos/Encapsulamento/RetanguloNew.java
@@ -1,5 +1,31 @@
 package Exercícios;
 
 public class RetanguloNew {
+    private double altura;
+    private double base;
 
+    public RetanguloNew(double altura, double base) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double calcularArea() {
+        return base * altura;
+    }
 }

f8856412a0aa4c9774746ad776d50430c4eb3b7b
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-05-14 09:16:20 -0300

    Update Calculadora.java

--- Programação Orientada a Objetos/Polimorfismo/Calculadora/Calculadora.java
+++ Programação Orientada a Objetos/Polimorfismo/Calculadora/Calculadora.java
@@ -2,11 +2,25 @@ package Polimorfismo.Calculadora;
 
 public class Calculadora {
 
+    //somar
+    public int somar (int i, int j){
+        return i + j;
+    }
+
     public double somar(double d, int i){
         return d + i;
     }
 
     public double somar(int i, int j, int k){
         return i + j + k;
+    }
+
+    //multiplicar
+    public int multiplicar(int i, int j){
+        return i * j;
+    }
+
+    public double multiplicar (double d, double f){
+        return d * f;
     }
 }

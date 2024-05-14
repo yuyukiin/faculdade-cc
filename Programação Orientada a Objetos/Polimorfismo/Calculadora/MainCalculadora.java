45e7b1f5218316a6ed30a3d0d8f79fe38f64c463
Yuri Alexander <141375539+heyuyuyu@users.noreply.github.com>
2024-05-14 09:16:08 -0300

    Update MainCalculadora.java

--- Programação Orientada a Objetos/Polimorfismo/Calculadora/MainCalculadora.java
+++ Programação Orientada a Objetos/Polimorfismo/Calculadora/MainCalculadora.java
@@ -4,14 +4,31 @@ public class MainCalculadora {
     public static void main(String[] args) {
         Calculadora c = new Calculadora();
         
+        //somar
         double resultado1 = c.somar(2, 3);
         double resultado2 = c.somar(2, 3, 4);
         
+        //imprimir
         System.out.println("Resultado 1: " + resultado1);
         System.out.println("Resultado 2: " + resultado2);
         
-        double resultado3 = c.somar(2.5, 7);
+        //sobrecarga
+        double resultado3 = c.somar(3, 7);
+
+        //imprimir
         System.out.println("Resultado 3: " + resultado3);
+
+        //multiplicar
+        double resultado4 = c.multiplicar(2,4);
+
+        //imprimir  
+        System.out.println("Resultado 4: " + resultado4);
+
+        //sobrecarga
+        double resultado5 = c.multiplicar(2.5, 3.5);
+        
+        //imprimir
+        System.out.println("Resultado 5: " + resultado5);
 
     }
 }

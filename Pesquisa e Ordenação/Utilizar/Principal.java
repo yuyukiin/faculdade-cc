716d271dd59a21582b1435cd4d2029ec2ff6a693
Yuri Alexander <141375539+yuyukiin@users.noreply.github.com>
2024-10-09 10:13:58 -0300

    Update Principal.java

--- Pesquisa e Ordenação/Utilizar/Principal.java
+++ Pesquisa e Ordenação/Utilizar/Principal.java
@@ -1,30 +1,43 @@
 package Aula;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeSet;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        ArrayList <Integer> listaDesordenada = new ArrayList<>();
-        Util.preencheBagunca(listaDesordenada,5000);
         ArrayList <Integer> listaOrdenada = new ArrayList<>();
-        Util.PreencheOrdenado(listaOrdenada, 5000);
+        Util.PreencheOrdenado(listaOrdenada, 2000000);
+
+        ArrayList <Integer> listaDesordenada = new ArrayList<>();
+        Util.preencheBagunca(listaDesordenada,2000000);
+
+        TreeSet<Integer> arvore = new TreeSet<>();
+        Util.preencheArvore(arvore,2000000);
+        System.out.println(arvore.contains(2000000-1));
         
         long tempoInicio = System.nanoTime();
-        System.out.println("Iniicou tempo de pesquisa...");
-        System.out.println(listaOrdenada.contains(2000-1));
+        System.out.println("Iniicou tempo de pesquisa lista ordenada ARVORE...");
+        System.out.println(arvore.contains(2000000-1));
+
         System.out.println("tempo (ms) cotains ordenada: "+ (System.nanoTime()- tempoInicio));
         
         tempoInicio = System.nanoTime();
-        System.out.println("Iniicou tempo de pesquisa...");
-        System.out.println(listaDesordenada.contains(2000-1));
-        System.out.println("tempo (ms) cotains ordenada: "+ (System.nanoTime()- tempoInicio));
+        System.out.println("Inicou tempo de pesquisa lista desordenada SORT...");
+        System.out.println(listaDesordenada.contains(2000000-1));
+        Collections.sort(listaDesordenada);
+        System.out.println("tempo (ms) cotains desordenada: "+ (System.nanoTime()- tempoInicio));
         
+
+        /*
         tempoInicio = System.nanoTime();
         System.out.println("Iniicou tempo de pesquisa...");
         System.out.println(Util.pesquisaBinarioContain(1000000,listaOrdenada));
         System.out.println("tempo (ms) cotains ordenada: "+ (System.nanoTime()- tempoInicio));
+         */
+
         
     }
 }

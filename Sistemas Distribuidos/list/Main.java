3e202318b08a41f6092da9a19ffd657660c8576b
Yuri Alexander <141375539+yuyukiin@users.noreply.github.com>
2025-08-31 21:47:22 -0300

    Update Main.java

--- Sistemas Distribuidos/list/Main.java
+++ Sistemas Distribuidos/list/Main.java
@@ -2,25 +2,22 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int totalListas = 10;      // m = número de listas
-        int tamanhoLista = 1000;   // n/m = quantidade de números em cada lista
+        int totalListas = 10;      
+        int tamanhoLista = 1000;   
 
         List<List<Integer>> listaDeListas = new ArrayList<>();
         List<PopulaLista> threads = new ArrayList<>();
 
-        // cria listas vazias
         for (int i = 0; i < totalListas; i++) {
             listaDeListas.add(new ArrayList<>());
         }
 
-        // popula cada lista com uma thread
         for (int i = 0; i < totalListas; i++) {
             PopulaLista t = new PopulaLista(listaDeListas.get(i), tamanhoLista);
             threads.add(t);
             t.start();
         }
 
-        // aguarda todas as threads finalizarem
         for (PopulaLista t : threads) {
             try {
                 t.join();
@@ -29,7 +26,6 @@ public class Main {
             }
         }
 
-        // calcula a média de todas as listas
         long soma = 0;
         int totalNumeros = 0;
 

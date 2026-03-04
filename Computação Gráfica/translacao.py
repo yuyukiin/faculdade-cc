488c9e17a7801834f047ce146e93c48c7a283d09
Yuri Alexander <141375539+yuyukiin@users.noreply.github.com>
2026-03-04 10:32:09 -0300

    Update translacao.py

--- Computação Gráfica/translacao.py
+++ Computação Gráfica/translacao.py
@@ -1,21 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-"""
-Agora complete o algoritmo anterior para o exemplo do Slide 8, que tem 3
-pontos, ao invés de 2 apenas.
-p1(6,8)
-p2(4,5)
-p3(8,5)
-(tx, ty) = (3,-4)
-p1'(?,?) = (9,4)
-p2'(?,?) = (7,1)
-p3'(?,?) = (11,1)
-
-
-"""
-
-# Função para calcular a translação dos pontos
 def translacao(pontos, Tx, Ty):
     pontos_transladados = []
     for ponto in pontos:
@@ -24,15 +9,11 @@ def translacao(pontos, Tx, Ty):
         pontos_transladados.append((x_u, y_u))
     return pontos_transladados
 
-# --- Preparação dos Dados ---
-
-# Pontos originais (definidos como tuplas)
 p1 = (6, 8)
 p2 = (4, 5)
-p3 = (8,5)
+p3 = (8, 5)
 array_pontos = [p1, p2, p3]
 
-# Vetor de translação
 Tx = 3
 Ty = -4
 
@@ -44,27 +25,15 @@ lista_y_orig = [p1[1], p2[1], p3[1], p1[1]]
 lista_x_trans = [p[0] for p in pontos_transladados] + [pontos_transladados[0][0]]
 lista_y_trans = [p[1] for p in pontos_transladados] + [pontos_transladados[0][1]]
 
-# --- Plotagem do Gráfico ---
-
-# Plotar os pontos originais
 plt.plot(lista_x_orig, lista_y_orig, 'bo-', label='Pontos originais')
-plt.plot(lista_x_trans, lista_y_trans, 'ro-', label='Transladado (Triângulo)')
+plt.plot(lista_x_trans, lista_y_trans, 'ro-', label='Transladado (Triangulo)')
 
-# Organizar as coordenadas transladadas para o plot
-lista_x = [pontos_transladados[0][0], pontos_transladados[1][0]]
-lista_y = [pontos_transladados[0][1], pontos_transladados[1][1]]
-
-# Plotar os pontos transladados
-plt.plot(lista_x, lista_y, 'ro-', label='Pontos transladados')
-
-# Configurações de exibição
-plt.xlim(-2, 20) 
-plt.ylim(-2, 20) 
+plt.xlim(-2, 15) 
+plt.ylim(-2, 15) 
 plt.xlabel('Eixo X')
 plt.ylabel('Eixo Y')
-plt.title('Translação de Pontos no Plano Cartesiano')
+plt.title('Translacao de Pontos no Plano Cartesiano')
 plt.grid(True)
 plt.legend()
 
-# Mostrar o gráfico
 plt.show()
